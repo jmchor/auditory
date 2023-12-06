fc12acbe09c2158131074d7ac937631ef33a8a41
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-06 08:23:42 +0100

    search route creates a db record

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -14,10 +14,28 @@ router.get("/:id", async (req, res, next) => {
             [id]
         );
 
-        console.log(track.rows);
+        const artist = track.rows[0].artist
+        const song = track.rows[0].track
+        const query = `${artist} ${song}`
 
-        // Respond with the track data
-        res.json({ success: true, track: track.rows[0] });
+       const youtubeURL = await searchYouTubeVideos(query)
+
+
+
+       const updateResult = await pool.query(
+        'UPDATE songs SET youtube_url = $1 WHERE trackId = $2 RETURNING *',
+        [youtubeURL, id]
+    );
+
+    // Check if the update was successful
+    if (updateResult.rows.length > 0) {
+        console.log('YouTube URL updated:', updateResult.rows[0]);
+        res.json({ success: true, track: updateResult.rows[0] });
+    } else {
+        console.log('No matching track found for trackId:', id);
+        res.status(404).json({ success: false, error: 'Track not found' });
+    }
+
     } catch (error) {
         console.error('Error fetching track:', error);
         res.status(500).send('Internal Server Error');
