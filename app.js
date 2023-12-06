6472603e487fdd9fa2e3d1694f1754d0c9dc249a
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-06 13:31:07 +0100

    Add artists routes

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use("/albums", albumRoutes);
 const searchRoutes = require("./routes/search.routes");
 app.use("/songs", searchRoutes);
 
+const artistsRoutes = require("./routes/artists.routes");
+app.use("/artists", artistsRoutes);
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
