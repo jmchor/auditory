62dd7b3a65837afc9f95f4a6bb2c9dcc428634ea
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-07 17:09:06 +0100

    Update key and exports

--- service.js
+++ service.js
@@ -3,7 +3,7 @@ const getAccessToken = require("./accessToken")
 
 const clientID = process.env.SPOTIFY_CLIENT_ID
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
-  
+
   // Function to get playlists and their IDs
   async function getPlaylists(accessToken) {
     try {
@@ -12,9 +12,9 @@ const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
           Authorization: `Bearer ${accessToken}`,
         },
       });
-  
+
       const allPlaylists = response.data.items.map(playlist => ({ id: playlist.id, name: playlist.name }));
-     
+
       return allPlaylists
 
     } catch (error) {
@@ -23,7 +23,7 @@ const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
     }
   }
 
-  
+
   // Function to convert MS to m and s
   function millisToMinutesAndSeconds(millis) {
     let minutes = Math.floor(millis / 60000);
@@ -31,7 +31,7 @@ const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
   }
 
-  
+
   // Function to get playlist details and tracks for a given playlist ID
 async function getPlaylistDetails(accessToken, playlistId) {
     const allTrackInfo = [];
@@ -41,59 +41,59 @@ async function getPlaylistDetails(accessToken, playlistId) {
           Authorization: `Bearer ${accessToken}`,
         },
       });
-  
+
       response.data.items.map(item => {
         const track = item.track;
-        const trackId = track.id;
+        const track_id = track.id;
         const artists = track.artists.map(artist => artist.name).join(', '); // Join multiple artists if there are any
         const artistId = track.artists.map(artist => artist.id)[0];
         const {name, id} = track.album
-        
+
         const duration = track.duration_ms;
         const durationMinutesSeconds = millisToMinutesAndSeconds(duration)
 
-        const songObject = { artist: artists, spotifyID: artistId, track: track.name, trackId, duration: durationMinutesSeconds, albumName: name, albumID: id }
-  
+        const songObject = { artist: artists, artist_id: artistId, track: track.name, track_id, duration: durationMinutesSeconds, albumName: name, albumID: id }
+
         allTrackInfo.push(songObject); // Push the songObject to the array
-  
+
       });
-  
+
       return allTrackInfo
-      
+
     } catch (error) {
       console.error(`Error getting playlist details for playlist ${playlistId}:`, error.message);
       return [];
     }};
-  
+
 
 
 // Main function
 async function main() {
     // Get access token
     const accessToken = await getAccessToken();
-  
+
     // Proceed only if access token is obtained
     if (accessToken === null) {
       console.log('Unable to obtain access token. Exiting.');
       process.exit(1);
     }
-  
+
     // // Get playlists
     const playlists = await getPlaylists(accessToken);
     const allTracks =[];
-  
+
     // Iterate through playlists
     for (const playlist of playlists) {
-  
+
       const tracks = await getPlaylistDetails(accessToken, playlist.id);
       allTracks.push(...tracks)
-  
+
   // Add tracks to the playlist object
       // playlist.tracks = tracks;
       // return tracks;
   }
   return allTracks;
-  
+
   }
 
-  module.exports = main;
+  module.exports = {main, millisToMinutesAndSeconds};
