d1cf6e617b742e55e5736c8f41027cb991c15412
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-09 21:15:57 +0100

    Update to fetch all albumIds for one artist

--- routes/albums.routes.js
+++ routes/albums.routes.js
@@ -32,6 +32,30 @@ router.get('/album_ids', async (req, res, next) => {
 });
 
 //===================================================================================================
+//================================= FETCH ALL ALBUMIDS FOR AN ARTIST ID =============================
+//===================================================================================================
+
+router.get('/for/:artist_id', async (req, res, next) => {
+	const { artist_id } = req.params;
+
+	try {
+		// Assuming you have a column named 'album_ids' in the 'artists' table
+		const query = 'SELECT album_ids FROM artists WHERE artist_id = $1';
+		const result = await pool.query(query, [artist_id]);
+
+		if (result.rows.length > 0) {
+			albumIDsArray = result.rows[0].album_ids;
+			res.json(albumIDsArray);
+		} else {
+			res.json([]); // or handle the case where no artist with the specified artist_id is found
+		}
+	} catch (error) {
+		console.error('Error fetching album IDs:', error);
+		res.status(500).send('Internal Server Error');
+	}
+});
+
+//===================================================================================================
 //====== USE ALBUMIDS FROM WHEREVER TO FETCH BULK API ALBUM DATA AND INSERT INTO ALBUM TABLE ========
 //===================================================================================================
 // This API call contains TRACK_IDS, since it's the SINGLE-ALBUM-API and uses a different function
@@ -43,99 +67,7 @@ router.post('/with-trackids', async (req, res, next) => {
 		const delayDuration = 1000;
 		let iterationCounter = 0;
 
-		albumIDsArray = [
-			'4Vv2AV5fDsjzOume79vXqK',
-			'4uG8q3GPuWHQlRbswMIRS6',
-			'7DOfHJBR8vjFnOeg2q3ich',
-			'6deiaArbeoqp1xPEGdEKp1',
-			'2Sjpjtk2LrVs4djFTr2RmJ',
-			'4JmrIJbpHWHZxOJg4awGh4',
-			'2qnnUYHObTYAYhUEL4MC43',
-			'58alCatewkjNm9IM1Ucj67',
-			'28q2N44ocJECgf8sbHEDfY',
-			'13sWe0tYKenwLd2t25bHIi',
-			'0CA2EVHhRPR5VPV78KZw89',
-			'5OubC3vkQNuA3rVu6iD6xa',
-			'2XdVmzy6wBoXxqmvBrPB0x',
-			'3rxF05Aux0QTrN533Kjc91',
-			'7IDywTRaCI8qzS3X8tNU3x',
-			'1QZi8laY96nhaeGSklvN4D',
-			'3sxJpkdsLs0F2D3cSsrYEB',
-			'3J6Tc1cPw8LovadG8LkcFP',
-			'2QgGoL5VSQhPHudTObS7zK',
-			'1tWgv9v78StWukBRBVNyxA',
-			'2DumvqHl78bNXuvU9kQfPN',
-			'2UqJjz5eMYRzbbKToD3Peh',
-			'42QVkdlfEk9uaG0NboeKpq',
-			'652N05EcNH1a4bIlUixQE2',
-			'3R6Z3Hp8EMdhgkA5t89NiL',
-			'6AnpM7DFltgJLIjlToxYJ1',
-			'7IoPLwGpntUE7VaEXEU67i',
-			'0NufsuTuf3U0BY0p6jFdxV',
-			'3CFswsRArMIduymEMdBaHi',
-			'4KZGe18wZJbXL6JLW4KyLc',
-			'2AcYgdvD5Fmfa27CsNoCAS',
-			'3dLKM8bD8R3H3XnSOXGjTF',
-			'1H9g6j4Wwj6wh6p8YHVtkf',
-			'70GAqt2avSiKBJ2IEymou1',
-			'67v63ubEsvDUQkYMzI7A9t',
-			'72qrnM4yUNMDDlWiqKc8iY',
-			'4J79cxsmRqZbI0BKmNFDy2',
-			'2TAeZbcOfhZnaL2wLNtPyj',
-			'4ZDBQSIDIZRUBOG2OHcN3T',
-			'6N9IuJzAOwbzPx9oM0ar0j',
-			'6XiOgru3HoBvQt2oybb383',
-			'6GLHwIp1K3u1zdLOdPRG0W',
-			'2BCI4Oj05SiqooWWBqUqjT',
-			'2kHGB7mHwxG9iJ9X9Ex3Q3',
-			'6Ef4yClroC8pMGktYOUd5n',
-			'6jmesEKzKTLAa3oI96jWow',
-			'4X87hQ57jTYQTcYTaJWK5w',
-			'2fThnKRzs6iudIAzHP9VOa',
-			'52kEF3wvknVbIAk28VStyL',
-			'1ja2qzCrh6bZykcojbZs82',
-			'6ZizJ3rk1Eok7JOKL6UMwv',
-			'6SS8I9qRTwh0tMdAYoTbat',
-			'1S7mumn7D4riEX2gVWYgPO',
-			'6Rk2VghhfqeYrgKxS21eBh',
-			'55tK4Ab7XHTOKkw0xDz3AA',
-			'1n8QZFcwx5aQ2LIIlj0iYe',
-			'6zWmAA39H0c4SjnD4MB1Dm',
-			'6bzqUICBtzct7TiQC8if9s',
-			'5yrouz3mmUWSsCufl1tLUJ',
-			'08mPxuP35Db56jUUgRvGFs',
-			'5nbEy2Nx0M5S8Mtnf3pCEw',
-			'7xCSZZQEnMSSpzTgWzrPhG',
-			'2o2G49EPi4lua5zgxUKhLL',
-			'6GDFV4kWsQDYuah6tSqmu2',
-			'4NDM4HzjJYI27hwvwmKj6s',
-			'44Ig8dzqOkvkGDzaUof9lK',
-			'1cM3r0WQZWNkCpEbmFjLln',
-			'1pL7ML56e7X0a0FdEGHO5W',
-			'5l5m1hnH4punS1GQXgEi3T',
-			'2bjTVISSsvwia7uxrrEsuQ',
-			'2x2VoPa1pG2jSElA73a9Xa',
-			'25JdFEbKXocMQyJdlBzD5d',
-			'0YCraVqAWvJHiBYP2AXgV6',
-			'16VJRQuM2mXHkuppi3SNAh',
-			'7IFxOeu4nvoZlVPkE8GCf1',
-			'7gesblPNpHD2mhMQyKd6MS',
-			'2QAUtHFOqfnkOVZgRKXlGr',
-			'1ijkFiMeHopKkHyvQCWxUa',
-			'03GeIP6sDuMEQydiQgwJ9M',
-			'2HTLCIs9enszF8Wj4fm3IP',
-			'3nUNxSh2szhmN7iifAKv5i',
-			'1gTvJG5YnrTiwr0uDuzaoA',
-			'4DaRuYVybHMarhtdNWN0Bv',
-			'3rHeq4F5wnaLBjNtuz7Yvh',
-			'3gz1CZk5wFg2inBuUWDa8x',
-			'3REUXdj5OPKhuDTrTtCBU0',
-			'1jWoSWzUjyq2v2jnmRv3p8',
-			'3zMAFbz27Bd7gtmPFAq46b',
-			'6LByYePPTwk8mGhg01em17',
-			'0hqgU6C8MRpj88oJzazVyI',
-			'5r4qa5AIQUVypFRXQzjaiu',
-		];
+		console.log('albumIDsArray:', albumIDsArray);
 
 		// Fetch albums and push them to albumObjects
 		for (const albumID of albumIDsArray) {
@@ -170,6 +102,7 @@ router.post('/with-trackids', async (req, res, next) => {
 					trackCount,
 					album_type,
 					track_ids,
+					artist_Id,
 				} = albumObject;
 
 				// Check if the album already exists in the database
@@ -185,6 +118,7 @@ router.post('/with-trackids', async (req, res, next) => {
 						existingData.label !== label ||
 						existingData.albumname !== albumName ||
 						existingData.artist !== artist ||
+						existingData.artist_id !== artist_Id ||
 						existingData.tracks !== trackCount ||
 						existingData.releasedate !== releaseDate ||
 						existingData.album_type !== album_type ||
@@ -194,7 +128,7 @@ router.post('/with-trackids', async (req, res, next) => {
 					) {
 						// Update the existing record
 						const result = await pool.query(
-							'UPDATE albums SET label = $2, albumname = $3, artist = $4, tracks = $5, releasedate = $6, album_type = $7, track_ids = $8 WHERE albumid = $1 RETURNING *',
+							'UPDATE albums SET label = $2, albumname = $3, artist = $4, tracks = $5, releasedate = $6, album_type = $7, track_ids = $8, artist_Id = $9 WHERE albumid = $1 RETURNING *',
 							[
 								albumId,
 								label,
@@ -204,6 +138,7 @@ router.post('/with-trackids', async (req, res, next) => {
 								releaseDate,
 								album_type,
 								track_ids,
+								artist_Id,
 							]
 						);
 
