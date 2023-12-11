3e1d64524130e0d0ab556cbbdbe8726d86d28e6d
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-11 15:47:52 +0100

    Update server configuration

--- config/index.js
+++ config/index.js
@@ -1,31 +1,30 @@
-const express = require("express")
+const express = require('express');
 
-const logger = require("morgan");
+const logger = require('morgan');
 
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 
-const cors = require("cors");
+const cors = require('cors');
 
-const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
-
+const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:5173';
 // Middleware configuration
 module.exports = (app) => {
-  // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-  // Services like heroku use something called a proxy and you need to add this to your server
-  app.set("trust proxy", 1);
+	// Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
+	// Services like heroku use something called a proxy and you need to add this to your server
+	app.set('trust proxy', 1);
 
-  // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: [FRONTEND_URL]
-    })
-  );
+	// controls a very specific header to pass headers from the frontend
+	app.use(
+		cors({
+			origin: [FRONTEND_URL],
+		})
+	);
 
-  // In development environment the app logs
-  app.use(logger("dev"));
+	// In development environment the app logs
+	app.use(logger('dev'));
 
-  // To have access to `body` property in the request
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+	// To have access to `body` property in the request
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
+	app.use(cookieParser());
 };
