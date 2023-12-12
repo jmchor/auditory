84c59e3e75a5c32cfc9c8131fb78059167cc4c9f
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-12 10:02:55 +0100

    Add edit routes

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.use('/tracks', tracksRoutes);
 const catchAllRoutes = require('./routes/catch-all.routes');
 app.use('/catchall', catchAllRoutes);
 
+const editRoutes = require('./routes/edit.routes');
+app.use('/edit', editRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
