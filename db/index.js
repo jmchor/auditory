fc3bf90612002739c6b0eff77fadbb7f36730866
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-19 13:06:07 +0100

    update to use public connection string

--- db/index.js
+++ db/index.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_DATABASE,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
-});
+// const pool = new Pool({
+// 	user: process.env.DB_USER,
+// 	host: process.env.DB_HOST,
+// 	database: process.env.DB_DATABASE,
+// 	password: process.env.DB_PASSWORD,
+// 	port: process.env.DB_PORT,
+// });
+
+// for prod
+
+const connectionString = 'postgres://fmbihinn:ppzvbqVXSnrQEyzwOvbL0PuRF_Cbrmd8@flora.db.elephantsql.com/fmbihinn';
+const pool = new Pool({ connectionString });
 
 module.exports = pool;
