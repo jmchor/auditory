9a0c725ea03e0f5112f0578868a54df8d87531f3
Johannes Chorzempa <jchorzempa@posteo.org>
2023-12-19 15:50:28 +0100

    Update DB access

--- db/index.js
+++ db/index.js
@@ -1,13 +1,16 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_DATABASE,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
-});
+// const pool = new Pool({
+// 	user: process.env.DB_USER,
+// 	host: process.env.DB_HOST,
+// 	database: process.env.DB_DATABASE,
+// 	password: process.env.DB_PASSWORD,
+// 	port: process.env.DB_PORT,
+// });
+
+const connectionString = process.env.DB_URL;
 
 // for prod
+const pool = new Pool({ connectionString });
 
 module.exports = pool;
